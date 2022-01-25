7ae41a25e49c68e928c45fb43dc200adf674444c
nao-18 <kyahyo17@yahoo.co.jp>
2022-01-25 23:44:24 +0900

    logfile出力を追加

--- server/main.go
+++ server/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/nao-18/test-todo-app/config"
+	"log"
 )
 
 func main() {
@@ -10,4 +11,6 @@ func main() {
 	fmt.Println(config.Config.SQLDriver)
 	fmt.Println(config.Config.DbName)
 	fmt.Println(config.Config.LogFile)
+
+	log.Println("test")
 }
